91f013a7b9966185c9e4f037b7be3f7581024c59
Taylor <taylor.glas@gmail.com>
2016-08-27 10:15:34 -0700

    Second update

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 
 $(document).ready(function(){
-	var $container = $("<div class='container'></div>")
-	$container.appendTo('body');
-
-	$('.container').ready(function(){
-		var rows = prompt("how many rows do you want?");
-		// var units = prompt("how many units do you want in each row?")
-		for(var i = 0 ; i < rows ; i++){  //i needs to eventually equal the user's desired number of rows input.
-		for (var x = 0; x < 4; x++) {  //x needs to eventually equal the user's desired number of colums/100% for width of each unit.
-			var $newdiv = $("<div class='div'></div>")
-			$newdiv.appendTo('.container');
-			$('.div').css('width' , 'units/100%')
+	$('.reset').click(function(){  											//Create a reset button
+		$('.block').removeClass('color');									//Remove the color from blocks
+		var squares = prompt('how many squares per side would you like?');	//Ask user how many squares they want		
+		var squareWidth = (960/squares);										//Determine the width of each square based on user prompt
+		
+		for (var i = 0; i < 10 ; i++) {
+			var $block = $("<div class='block'></div>");
+			$($block).css('width' , 'squareWidth * squareWidth')
+			$('.container').append($block);
 		};
-	}
-	console.log(rows);
-	console.log(units)
-
 	});
-})
 
+	$('.block').hover(function(){
+		$(this).addClass('color')
+	});
 
-// $(document).ready(function() {
-//     for(var x = 0; x < 16; x++) {
-//         for(var y = 0; y < 16; y++) {
-//             var unit = $("<div class='unit'></div>");
-//             unit.appendTo('#container');
-//         }
-//     }
-// });
+});
+
