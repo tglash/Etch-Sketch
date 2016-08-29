f172cc6ed78c3469d9aca7b876b11aa1ddd6fc13
Taylor <taylor.glas@gmail.com>
2016-08-28 18:08:20 -0700

    third commit

--- app.js
+++ app.js
@@ -3,17 +3,29 @@ $(document).ready(function(){
 	$('.reset').click(function(){  											//Create a reset button
 		$('.block').removeClass('color');									//Remove the color from blocks
 		var squares = prompt('how many squares per side would you like?');	//Ask user how many squares they want		
-		var squareWidth = (960/squares);										//Determine the width of each square based on user prompt
-		
-		for (var i = 0; i < 10 ; i++) {
-			var $block = $("<div class='block'></div>");
-			$($block).css('width' , 'squareWidth * squareWidth')
-			$('.container').append($block);
-		};
-	});
+		var squareWidth = (960/squares);									//Determine the width of each square based on user prompt
+		var squareHeight = squareWidth;
+		var squareNum = (960/squareWidth);
 
-	$('.block').hover(function(){
-		$(this).addClass('color')
+
+		for (var x = 0; x < squareNum; x++) {
+			var $block = $("<div class='block'></div>");
+			$($block).css({'width' : squareWidth , 'height' : squareHeight})
+			$('.container').append($block);
+			
+			for (var i = 0; i < squareNum ; i++) {
+				var $block = $("<div class='block'></div>");
+				$($block).css({'width' : squareWidth , 'height' : squareHeight})
+				$('.container').append($block);
+				console.log("derp")
+		};	
+		};
+	
+		$('.block').hover(function(){
+			$(this).addClass('color');
+			console.log(squareNum);
+		});
+
 	});
 
 });
